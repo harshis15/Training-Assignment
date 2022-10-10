c411d275da77123bc5d098d891c5e511f518ef5a
harshis15 <harshithas2000@gmail.com>
2022-10-10 19:55:21 +0530

    New Project created

---
+++ NewProject/NewProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewProject
+//
+//  Created by Harshitha Srikanth on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        new line
+    }
+
+
+}
+
